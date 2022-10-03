64a941ec6d8cd36515333ad414d3c3790d61fc82
DispicableLee <lee.robert053@gmail.com>
2022-10-03 14:32:57 -0400

    extra

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", (e)=>{
-    //get json data
-
+// ================================= json-server --watch db.json ===============================
+//=============================== getting drugs data
+    function getDrugs(){
+        fetch("http://localhost:3000/My-Favorite-Drugs")
+            .then(res=> res.json())
+            .then(json=>{
+                console.log(json)
+            })
+        
+    }
+    getDrugs();
+//posting 
 
     //add "purchase" event listener
 
