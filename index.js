68e7da86ba249b7646e3d5f61285fa6aa5f06266
DispicableLee <lee.robert053@gmail.com>
2022-10-03 14:47:48 -0400

    getting closer

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const nameH1 = document.getElementById("name")
+const commentH1 = document.getElementById("comment")
+const inventoryH2 = document.getElementById("inventory")
+const originH3 = document.getElementById("origin")
+const priceH4 = document.getElementById("price")
+
 document.addEventListener("DOMContentLoaded", (e)=>{
 // ================================= json-server --watch db.json ===============================
 //=============================== getting drugs data
