8b6cfed0566df275d2507d744e18d0878b7a1c74
Cesar Hernandez <chema27281@gmail.com>
2022-10-06 13:57:57 -0400

    Finishing touches

--- src/index.js
+++ src/index.js
@@ -6,56 +6,89 @@ const displayPrice = document.getElementById("display-price")
 const displayInventory = document.getElementById("display-inventory")
 const displayOrigin = document.getElementById("display-origin")
 const form = document.querySelector(".form-info")
-document.addEventListener("DOMContentLoaded", (e)=>{
+const itemForm = document.querySelector('.item-form')
+const newAmount = document.querySelector('#quantity')
+const currentAmount = document.querySelector("#amount-x")
+document.addEventListener("DOMContentLoaded", (e) => {
     console.log(form)
     // ================================= json-server --watch db.json ===============================
     //=============================== getting drugs data
-    function getDrugs(){
-        fetch("http://localhost:3000/products")
-        .then(res=> res.json())
-        .then(json=>{
-            for(let i = 0; i<json.length; i++){
-//============================= setting constants from data ===================================
-                const name = json[i].name;
-                const nameCopy = name
-                const price = json[i].price;
-                const image = json[i].image;
-                const inventory = json[i].inventory;
-                const origin = json[i].Origin;
-                console.log(origin)
-//=========================== declaring html elements in DOM ============================
-                const newLi = document.createElement("li")
-                const nameH1 = document.createElement("h1")
-                    nameH1.innerText = name;
-//======================= info-displayed event listener =======================================
-                nameH1.addEventListener("click", (e)=>{
-                    e.preventDefault();
-                    displayName.textContent = nameCopy;
-                    displayImage.src = image
-                    displayPrice.textContent = price
-                    displayInventory.textContent = inventory
-                    displayOrigin.textContent = origin
-                })
-                newLi.append(nameH1)
-                info.append(newLi)
 
-                }
+    fetch("http://localhost:3000/products")
+        .then(res => res.json())
+        .then(data => getDrugs(data))
+
+    //============================= setting constants from data ===================================
+    function getDrugs(drug) {
+        drug.forEach((drugs) => {
+            const name = drugs.name;0
+            const nameCopy = name
+            const price = drugs.price;
+            const image = drugs.image;
+            const inventory = drugs.inventory;
+            const origin = drugs.Origin;
+            console.log(origin)
+            //=========================== declaring html elements in DOM ============================
+            const newLi = document.createElement("li")
+            const nameH1 = document.createElement("h1")
+            nameH1.innerText = name;
+            //======================= info-displayed event listener =======================================
+            nameH1.addEventListener("click", (e) => {
+                e.preventDefault();
+                displayName.textContent = `Name: ${nameCopy}`;
+                displayImage.src = image
+                displayPrice.textContent = `Price: $${price}`
+                displayInventory.textContent = `Inventory: ${inventory}`
+                displayOrigin.textContent = `Origin: ${origin}`
             })
-        
-    }
-    getDrugs();
-//posting 
+            newLi.append(nameH1)
+            info.append(newLi)
 
-//=========================== "buy" event listener =====================================   
-        form.addEventListener("submit", (e)=>{
-            e.preventDefault();
-            console.log("i have been clicked")
-            alert("THIS IS THE DEA\r\nYou have been caught!")
         })
-
-
-
+    }
 })
 
 
-//comment
+//posting 
+
+//=========================== "buy" event listener =====================================   
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("i have been clicked")
+    alert("THIS IS THE DEA\r\nYou have been caught!")
+})
+
+
+
+
+
+
+//=========================== "light/dark mode" event listener =====================================   
+icon.addEventListener('mouseover', (e) => {
+    e.preventDefault();
+    icon.onmouseover = function () {
+        document.body.classList.toggle("dark-mode");
+        if (document.body.classList.contains("dark-mode")) {
+            icon.src = "images/sun.png";
+        } else {
+            icon.src = "images/moon.png";
+        }
+    }
+}
+)
+//=========================== "Add to Cart Event Listener"=====================================
+ itemForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const newAmount = e.target.quantity.value
+  const newAmountInteger = parseInt(newAmount)
+  const currentAmountString = currentAmount.textContent
+  const currentAmountInteger = parseInt(currentAmountString)
+  console.log(parseInt(newAmountInteger)+ parseInt(currentAmountInteger))
+  currentAmount.textContent = newAmountInteger
+ 
+
+
+
+ //debugger;
+
+ })
